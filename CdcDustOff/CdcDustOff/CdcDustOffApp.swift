c66914a04f8c7f7aa6a4c9072722d90a06003983
avis001 <k.sivavel@gmail.com>
2025-12-05 15:43:21 +0800

    main - Skeleton

---
+++ CdcDustOff/CdcDustOff/CdcDustOffApp.swift
@@ -0,0 +1,17 @@
+//
+//  CdcDustOffApp.swift
+//  CdcDustOff
+//
+//  Created by Sivasankar on 5/12/25.
+//
+
+import SwiftUI
+
+@main
+struct CdcDustOffApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
